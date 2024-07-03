f4e35036892c88f2a23838b6daea48bcc34a98e5
Scott Feichter <scottfeichter@gmail.com>
2024-07-03 14:39:54 -0700

    baseStats working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import Showcase from './Showcase';
 import './App.css';
+import BaseStats from './components/BaseStats';
 
 function App() {
+  const baseStatsPOJO = {
+    hp: 45,
+    attack: 49,
+    defense: 49,
+    spAttack: 65,
+    spDef: 65,
+    speed: 45,
+  };
+  console.log(BaseStats);
   return (
     <div className='main-wrapper background'>
       <Showcase />
+      <BaseStats baseStats={baseStatsPOJO}/>
     </div>
   );
 }
 
 export default App;
-
