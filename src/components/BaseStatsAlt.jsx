a2937462644ed5c7e6b1fc91e5df95adb32cedf7
Scott Feichter <scottfeichter@gmail.com>
2024-07-03 14:55:00 -0700

    added BaseStatsAlt.jsx

---
+++ src/components/BaseStatsAlt.jsx
@@ -0,0 +1,31 @@
+import "../BaseStats.css";
+
+function BaseStats({baseStats}) {
+    return (
+        <div className="base-stats">
+            <h1>BaseStats</h1>
+            <table>
+  <tbody>
+    <tr>
+      <td>Hit Points</td>
+      <td>{baseStats.hp}</td>
+    </tr>
+    <tr>
+      <td>Attack</td>
+      <td>{baseStats.attack}</td>
+    </tr>
+    <tr>
+      <td>Defense</td>
+      <td>{baseStats.defense}</td>
+    </tr>
+    <tr>
+      <td>Speed</td>
+      <td>{baseStats.speed}</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    )
+}
+
+export default BaseStats;
